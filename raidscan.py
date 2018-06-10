5f8c9e5435b72cf03a85738683f4c7c992ca8c12
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-06-10 15:07:04 +0200

    Update raidscan.py

--- raidscan.py
+++ raidscan.py
@@ -10,15 +10,22 @@ import os
 
 LOG = getLogger('')
 
+def exception_handler(loop, context):
+    loop.default_exception_handler(context)
+    exception = context.get('exception')
+    if isinstance(exception, ZeroDivisionError):
+        LOG.error("Found unhandeled exception. Stoping loops. Error:")
+        LOG.error(context)
+        loop.stop()
+
 if __name__ == '__main__':
     raid_nearby = raidnearby.RaidNearby()
     find_fort = findfort.FindFort()
     loop = asyncio.get_event_loop()
+    loop.set_exception_handler(exception_handler)
     tasks = [loop.create_task(raid_nearby.main()),
              loop.create_task(find_fort.findfort_main()),
              loop.create_task(crop.crop_task())
              ]
-    loop.run_until_complete(asyncio.wait(tasks))
+    loop.run_forever()
     loop.close()
-
-
