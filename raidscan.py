abfebd3415e06cbcd09d37d20da12be1fffe8cfa
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-06-10 18:31:04 +0200

    error handler now worlds

--- raidscan.py
+++ raidscan.py
@@ -14,8 +14,6 @@ def exception_handler(loop, context):
     loop.default_exception_handler(context)
     exception = context.get('exception')
     if isinstance(exception, Exception):
-        LOG.error("Found unhandeled exception. Stoping loops. Error:")
-        LOG.error(context)
         loop.stop()
 
 if __name__ == '__main__':
@@ -23,9 +21,8 @@ if __name__ == '__main__':
     find_fort = findfort.FindFort()
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
-    tasks = [loop.create_task(raid_nearby.main()),
-             loop.create_task(find_fort.findfort_main()),
-             loop.create_task(crop.crop_task())
-             ]
+    loop.create_task(raid_nearby.main())
+    loop.create_task(find_fort.findfort_main())
+    loop.create_task(crop.crop_task())
     loop.run_forever()
     loop.close()
