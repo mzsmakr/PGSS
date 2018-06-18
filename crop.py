0ce8d0e28b95af06c3e6b63c57c9af75961bfccb
mizu-github <AkiraMizushima@outlook.com>
2018-06-18 10:10:38 -0500

    crop_img function added

--- crop.py
+++ crop.py
@@ -11,68 +11,73 @@ import math
 #import pdb; pdb.set_trace()
 
 LOG = getLogger('')
+screenshot_path = Path(SCREENSHOT_SAVE_PATH)
+crop_save_path = os.getcwd() + '/process_img/'
+not_find_path = os.getcwd() + '/not_find_img/'
+web_server_path = os.getcwd()+'/webserver/'
+
+async def crop_img(filename)
+    filename = os.path.basename(fullpath_filename)
+    filename, ext = os.path.splitext(filename)
+    img = cv2.imread(str(fullpath_filename),3)
+
+    if img.dtype == 'uint16':
+        print('16 bit image')
+        img = (img / 256).astype('uint8')
+
+    if img is not None:
+        height, width, channels = img.shape
+        find_size_config = False
+        for size in RAID_NEARBY_SIZE:
+            if width == size['width'] and height == size['height']:
+                find_size_config = True
+                if ext == 'jpg':
+                    refB = 150
+                else:
+                    refB = 162                
+                refG = 194
+                refR = 252
+                dif1 = pow(img[size['comp_y']][size['comp_x']][0] - refB,2)
+                dif2 = pow(img[size['comp_y']][size['comp_x']][1] - refG,2)
+                dif3 = pow(img[size['comp_y']][size['comp_x']][2] - refR,2)
+                error = math.sqrt(dif1+dif2+dif3)
+                LOG.debug('comp error:{} B:{}({}) G:{}({}) R:{}({})'.format(error, img[size['comp_y']][size['comp_x']][0], refB, img[size['comp_y']][size['comp_x']][1], refG, img[size['comp_y']][size['comp_x']][2],refR))               
+#                        if (img[size['comp_y']][size['comp_x']] == [162, 193, 254]).all():
+                if error <= 10:
+                    LOG.info('screenshot with {}x{} found and raid'.format(width, height))
+                    crop1 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x1']:size['crop_x1']+size['crop_w']]
+                    crop2 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x2']:size['crop_x2']+size['crop_w']]
+                    crop3 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x3']:size['crop_x3']+size['crop_w']]
+                    crop4 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x1']:size['crop_x1']+size['crop_w']]
+                    crop5 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x2']:size['crop_x2']+size['crop_w']]
+                    crop6 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x3']:size['crop_x3']+size['crop_w']]
+                    cv2.imwrite(crop_save_path+filename+'_01.png', crop1)
+                    cv2.imwrite(crop_save_path+filename+'_02.png', crop2)
+                    cv2.imwrite(crop_save_path+filename+'_03.png', crop3)
+                    cv2.imwrite(crop_save_path+filename+'_04.png', crop4)
+                    cv2.imwrite(crop_save_path+filename+'_05.png', crop5)
+                    cv2.imwrite(crop_save_path+filename+'_06.png', crop6)
+                else:
+                    LOG.info('screenshot with {}x{} found without raid'.format(width, height))
+#                        os.remove(fullpath_filename)
+                break
+        if find_size_config == False:
+            shutil.copy2(fullpath_filename, not_find_path+'Screen_' + str(width) + 'x' + str(height) + '.' + ext)
+            LOG.info('No size matching config found in RAID_NEARBY_SIZE')
+            LOG.info('Check not_find_img directory and add RAID_NEARBY_SIZE in config for the screenshot iamge')
+        img = cv2.resize(img, None, fx = 0.35, fy = 0.35) 
+        save_file_path = web_server_path+'screenshot.jpg'
+        cv2.imwrite(save_file_path, img)
+        os.remove(fullpath_filename)
+        await asyncio.sleep(0.1) 
+    
 
 async def crop_task():
-    screenshot_path = Path(SCREENSHOT_SAVE_PATH)
-    crop_save_path = os.getcwd() + '/process_img/'
-    not_find_path = os.getcwd() + '/not_find_img/'
-    web_server_path = os.getcwd()+'/webserver/'
     LOG.info('Crop screenshot task started')
     LOG.info('Screenshot path:{}'.format(screenshot_path))
     while True:
         for fullpath_filename in screenshot_path.glob('*.jpg'):
-            filename = os.path.basename(fullpath_filename)
-            filename, ext = os.path.splitext(filename)
-            img = cv2.imread(str(fullpath_filename),3)
-
-            if img.dtype == 'uint16':
-                print('16 bit image')
-                img = (img / 256).astype('uint8')
-
-            if img is not None:
-                height, width, channels = img.shape
-                find_size_config = False
-                for size in RAID_NEARBY_SIZE:
-                    if width == size['width'] and height == size['height']:
-                        find_size_config = True
-                        refB = 150 #162
-                        refG = 194
-                        refR = 252
-                        dif1 = pow(img[size['comp_y']][size['comp_x']][0] - refB,2)
-                        dif2 = pow(img[size['comp_y']][size['comp_x']][1] - refG,2)
-                        dif3 = pow(img[size['comp_y']][size['comp_x']][2] - refR,2)
-                        error = math.sqrt(dif1+dif2+dif3)
-                        LOG.debug('comp error:{} B:{}({}) G:{}({}) R:{}({})'.format(error, img[size['comp_y']][size['comp_x']][0], refB, img[size['comp_y']][size['comp_x']][1], refG, img[size['comp_y']][size['comp_x']][2],refR))               
-#                        if (img[size['comp_y']][size['comp_x']] == [162, 193, 254]).all():
-                        if error <= 10:
-                            LOG.info('screenshot with {}x{} found and raid'.format(width, height))
-                            crop1 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x1']:size['crop_x1']+size['crop_w']]
-                            crop2 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x2']:size['crop_x2']+size['crop_w']]
-                            crop3 = img[size['crop_y1']:size['crop_y1']+size['crop_h'], size['crop_x3']:size['crop_x3']+size['crop_w']]
-                            crop4 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x1']:size['crop_x1']+size['crop_w']]
-                            crop5 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x2']:size['crop_x2']+size['crop_w']]
-                            crop6 = img[size['crop_y2']:size['crop_y2']+size['crop_h'], size['crop_x3']:size['crop_x3']+size['crop_w']]
-                            cv2.imwrite(crop_save_path+filename+'_01.png', crop1)
-                            cv2.imwrite(crop_save_path+filename+'_02.png', crop2)
-                            cv2.imwrite(crop_save_path+filename+'_03.png', crop3)
-                            cv2.imwrite(crop_save_path+filename+'_04.png', crop4)
-                            cv2.imwrite(crop_save_path+filename+'_05.png', crop5)
-                            cv2.imwrite(crop_save_path+filename+'_06.png', crop6)
-                        else:
-                            LOG.info('screenshot with {}x{} found without raid'.format(width, height))
-#                        os.remove(fullpath_filename)
-                        break
-                if find_size_config == False:
-                    shutil.copy2(fullpath_filename, not_find_path+'Screen_' + str(width) + 'x' + str(height) + '.png')
-                    LOG.info('No size matching config found in RAID_NEARBY_SIZE')
-                    LOG.info('Check not_find_img directory and add RAID_NEARBY_SIZE in config for the screenshot iamge')
-                img = cv2.resize(img, None, fx = 0.35, fy = 0.35) 
-                save_file_path = web_server_path+'screenshot.jpg'
-                cv2.imwrite(save_file_path, img)
-#                filename = os.path.basename(fullpath_filename)
-#                shutil.move(fullpath_filename, web_server_path+filename)
-                os.remove(fullpath_filename)
-                await asyncio.sleep(0.1) 
+            await prcess_img(filename)
         await asyncio.sleep(0.2) # task runs every 3 seconds
 
 
