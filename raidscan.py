2430e9016aa77e82eec1e7fe4889d0042ae27bb4
mizu <AkiraMizushima@outlook.com>
2018-06-20 23:11:34 -0500

    NO_FINDFORT argument option added to raidscan.py

--- raidscan.py
+++ raidscan.py
@@ -1,4 +1,5 @@
 import sys
+from sys import argv
 import datetime
 from logging import basicConfig, getLogger, FileHandler, StreamHandler, DEBUG, INFO, ERROR, Formatter
 import time
@@ -10,6 +11,13 @@ import os
 import concurrent.futures
 
 LOG = getLogger('')
+
+no_findfort = False
+if len(argv) >= 2:
+    if str(argv[1]) == 'NO_FINDFORT':
+        no_findfort = True
+        LOG.info('No findfort option selected.')
+        LOG.info('Run findfort.py separately to identify new unknown gyms')
 
 cpu_count = os.cpu_count()
 LOG.info('{} cpu cores found'.format(cpu_count))
@@ -23,13 +31,15 @@ def exception_handler(loop, context):
 
 if __name__ == '__main__':
     raid_nearby = raidnearby.RaidNearby()
-    find_fort = findfort.FindFort()
+    if no_findfort == False:
+        find_fort = findfort.FindFort()
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
     executor = concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count)
     loop.set_default_executor(executor)
     loop.create_task(raid_nearby.main())
-    loop.create_task(find_fort.findfort_main())
+    if no_findfort == False:    
+        loop.create_task(find_fort.findfort_main())
     loop.create_task(crop.crop_task())
     loop.run_forever()
     loop.close()
