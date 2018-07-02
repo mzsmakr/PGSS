ab897b786860c929142c51c06cf00666826a9300
mizu-github <AkiraMizushima@outlook.com>
2018-07-02 11:23:14 -0500

    check png file with matching.py

--- matching.py
+++ matching.py
@@ -2,44 +2,64 @@ import cv2
 import os
 
 def fort_image_matching(url_img_name, fort_img_name):
+    url_img_basename = os.path.basename(url_img_name)
+    url_img_basename, url_img_ext = os.path.splitext(url_img_basename)
+
     url_img = cv2.imread(url_img_name,3)
     fort_img = cv2.imread(fort_img_name,3)
 
     height, width, channels = url_img.shape
-    
-    if width > height:
-        scale = float(288/height)
-    else:
-        scale = float(288/width)
-        
-    height_f, width_f, channels_f = fort_img.shape
 
-    scale_fort = width_f/320
-        
-    url_img = cv2.resize(url_img,None,fx=scale*scale_fort, fy=scale*scale_fort, interpolation = cv2.INTER_NEAREST)
+    if url_img_ext == '.jpg':
+        if width > height:
+            scale = float(288/height)
+        else:
+            scale = float(288/width)
 
-    crop = fort_img[int(74*scale_fort):int(246*scale_fort),int(74*scale_fort):int(144*scale_fort)]
+        height_f, width_f, channels_f = fort_img.shape
 
-    # Calculate center of fort image(x=74, y=74, width=172, height=172) of fort_img
-    fi_center_x = ((246-74)*scale_fort)/2
-    fi_center_y = ((246-74)*scale_fort)/2
+        scale_fort = width_f/320
 
-    if crop.mean() == 255 or crop.mean() == 0:
-        return 0.0
+        url_img = cv2.resize(url_img,None,fx=scale*scale_fort, fy=scale*scale_fort, interpolation = cv2.INTER_NEAREST)
 
-    result = cv2.matchTemplate(url_img, crop, cv2.TM_CCOEFF_NORMED)
-    min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result)
-    
-    height, width, channels = url_img.shape
-    
-    ui_center_x = width/2-max_loc3[0]
-    ui_center_y = height/2-max_loc3[1]
- 
-    dif_x = abs(ui_center_x - fi_center_x)
-    dif_y = abs(ui_center_y - fi_center_y) 
-    
-    if dif_x > 5 or dif_y >5:
-        return 0.0
+        crop = fort_img[int(74*scale_fort):int(246*scale_fort),int(74*scale_fort):int(144*scale_fort)]
+
+        # Calculate center of fort image(x=74, y=74, width=172, height=172) of fort_img
+        fi_center_x = ((246-74)*scale_fort)/2
+        fi_center_y = ((246-74)*scale_fort)/2
+
+        if crop.mean() == 255 or crop.mean() == 0:
+            return 0.0
+
+        result = cv2.matchTemplate(url_img, crop, cv2.TM_CCOEFF_NORMED)
+        min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result)
+
+        height, width, channels = url_img.shape
+
+        ui_center_x = width/2-max_loc3[0]
+        ui_center_y = height/2-max_loc3[1]
+
+        dif_x = abs(ui_center_x - fi_center_x)
+        dif_y = abs(ui_center_y - fi_center_y)
+
+        if dif_x > 5 or dif_y >5:
+            return 0.0
+    else: # for png file
+        scale = float(width/(144-74))
+
+        height_f, width_f, channels_f = fort_img.shape
+        scale_fort = width_f / 320
+
+        url_img = cv2.resize(url_img, None, fx=scale * scale_fort, fy=scale * scale_fort,
+                             interpolation=cv2.INTER_NEAREST)
+
+        crop = fort_img[int(74*scale_fort):int(246*scale_fort),int(74*scale_fort):int(144*scale_fort)]
+
+        if crop.mean() == 255 or crop.mean() == 0:
+            return 0.0
+
+        result = cv2.matchTemplate(url_img, crop, cv2.TM_CCOEFF_NORMED)
+        min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result)
 
     return max_val3
 
