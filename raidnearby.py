76a80629929bc6ca96b67fa722f16d8ceb1730a5
mizu-github <AkiraMizushima@outlook.com>
2018-07-03 16:43:47 -0500

    fixed egg/boss detection error

--- raidnearby.py
+++ raidnearby.py
@@ -400,11 +400,10 @@ class RaidNearby:
         kernel = np.ones((2, 2), np.uint8)
         thresh1 = cv2.erode(thresh1, kernel, iterations=1)
         time_mean = cv2.mean(time_img, thresh1)
-        print(time_mean)
         if time_mean[2] > (time_mean[0]+50): # Red is greater than Blue+50
-            return True, thresh1
-        else:
             return False, thresh1
+        else:
+            return True, thresh1
 
     def checkHourMin(self, hour_min):
         hour_min[0] = hour_min[0].replace('O','0')
